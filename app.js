da7489e2cbad3c621f7e41e2212b59bdbcf525cd
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-02-09 15:57:18 +0700

    Revert "feat: add a few important tests using jest & supertest"
    This reverts commit 1715b322cec368d8ef930bc16c4e6c23a3576cc0.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import workflowRouter from "./routes/workflow.routes.js";
 
+import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 import cookieParser from "cookie-parser";
@@ -53,6 +55,11 @@ app.get("/health", (req, res) => {
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Subscription Tracker API!" });
+});
+
+await connectToDatabase();
+app.listen(PORT, async () => {
+  console.log(`Subscription Tracker is runnning on https://localhost:${PORT}`);
 });
 
 export default app;
